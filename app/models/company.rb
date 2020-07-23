c865a1d6d3baa203edbcb1653907c7265272befe
Abhishek <abhimahata24@gmail.com>
2020-07-23 20:41:55 +0530

    Added Custom Validation to company email Address (When Present)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,12 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
+  #validations
+  validates :name, :zip_code, presence: true
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet.\.com\z/,
+                    message: "must be a getmainstreet.com account" }, :allow_blank => true
+
+
   def self.destroy_the_record(company)
     result = {}
     begin
@@ -14,5 +20,6 @@ class Company < ApplicationRecord
 
     return result
   end
+
 
 end
